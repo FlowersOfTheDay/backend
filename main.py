352140a209023eb6a6238d41498ea1784ed2f82d
Shiwon Park <psw041115@gmail.com>
2024-05-25 15:28:47 +0000

    remove: prisma

--- main.py
+++ main.py
@@ -2,20 +2,14 @@ import os
 import dotenv
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from utils.prisma import prisma
 
 import routers
 
 # loading fron .env file
 dotenv.load_dotenv()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await prisma.connect()
-    yield
-    await prisma.disconnect()
 
-app = FastAPI(lifespan=lifespan, root_path=os.environ.get('BASE_URL', ''))
+app = FastAPI(root_path=os.environ.get('BASE_URL', ''))
 
 # connecting router
 app.include_router(routers.home.router)
