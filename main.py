075c03934d5881f3f22dd2d24e248d4faef7dfe0
Shiwon Park <psw041115@gmail.com>
2024-05-27 07:17:33 +0000

    using database

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
 import dotenv
+from utils.prisma import prisma
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 
 import routers
 
 # loading fron .env file
 dotenv.load_dotenv()
 
-app = FastAPI(root_path=os.environ.get('BASE_URL', ''))
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  await prisma.connect()
+  yield
+  await prisma.disconnect()
+  
+
+app = FastAPI(lifespan=lifespan,root_path=os.environ.get('BASE_URL', ''))
 
 # connecting router
 app.include_router(routers.home.router)
--- routers/chat.py
+++ routers/chat.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from prisma import enums
+from utils.prisma import prisma
+
 from llm.chat import build
 from llm.model import ChatGPTModel
 
@@ -12,12 +15,18 @@ class InputModel(BaseModel):
     description='필요한 꽃을 유추하기 위한 문장'
   )
 
-  context: str = Field(
-    alias='context',
-    description='대화의 문맥'
+  id: int | None = Field(
+    alias='id',
+    description='사용자 식별자, 처음 대화에서는 자동으로 생성되어 반환된다.',
+    default=None
   )
 
 class OutputModel(BaseModel):
+  id: int = Field(
+    alias='id',
+    description='사용자 식별자, input과 동일하게 반환된다.'
+  )
+
   output: str = Field(
     alias='output',
     description='생성된 문장'
@@ -32,12 +41,59 @@ model = ChatGPTModel()
 @router.post('/chat')
 async def chat(input: InputModel) -> OutputModel:
   chain = build(model.build())
+  if input.id == None:
+    log = await prisma.log.create(
+      data={
+        'model': 'gpt-4o'
+      }, include={
+        'chat': True
+      }
+    )
+  else:
+    log = await prisma.log.find_unique(where  = {
+      'id': input.id
+    }, include={
+      'chat': True
+    })
 
-  return OutputModel(
-    output=chain.invoke({
+  context = ''
+  for chat in log.chat:
+    if chat.role == 'USER':
+      context += f'사용자: {chat.text}\n'
+    else:
+      context += f'봇: {chat.text}\n'
+
+  output = chain.invoke({
        'input_context': f'''
-                * 현재까지의 대화: {input.context}
+                * 현재까지의 대화: {context}
                 * 사용자 입력: {input.chat}
             '''
-        }),
+        })
+
+  await prisma.chat.create(
+    data={
+      'role': enums.ChatRole.USER,
+      'text': input.chat,
+      'log': {
+        'connect': {
+          'id': log.id
+        }
+      }
+    }
+  )
+
+  await prisma.chat.create(
+    data={
+      'role': enums.ChatRole.AI,
+      'text': output,
+      'log': {
+        'connect': {
+          'id': log.id
+        }
+      }
+    }
+  )
+
+  return OutputModel(
+    output=output, id=log.id
   )
